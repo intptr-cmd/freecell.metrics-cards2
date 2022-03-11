a5f12051173f7ff927b9638e2e310f7148c79fb5
intptr-cmd <56723026+intptr-cmd@users.noreply.github.com>
2022-03-11 20:19:37 +0200

    Add files via upload

---
+++ FreeCellMetrics/Classes/TagReader.cs
@@ -0,0 +1,276 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Controls;
+
+namespace FreeCellMetrics.Classes
+{
+    //Tag="00S13"
+    public class TagReader
+    {
+        internal static void Populate(string startTag, Grid grid)
+        {
+            //set new tagS
+            //validate
+            //remove items
+
+            List<Button> buttons = new List<Button>();
+            foreach (Button item in grid.Children)
+            {
+                Button clickedBtn;
+
+                if (item.Content is Image)
+                    clickedBtn = (Button)((Image)item.Content).Parent;
+                else
+                    clickedBtn = (Button)item;
+
+                 buttons.Add(clickedBtn);
+            }
+
+           var startButton = buttons.GroupBy(x => x.Content.ToString())
+              .Where(g => g.Count() > 1)
+              .Select(y => y.Key)
+              .ToList();
+
+           if (startButton.Count > 0)
+           {
+               var removeButton = buttons.Where(b => b.Content.ToString() == startButton[0].ToString()).ToArray();
+               for (int i = 0; i < removeButton.Count(); i++ )
+                   buttons.Remove(((Button)removeButton[i]));
+           }
+           
+            grid.Children.RemoveRange(0, grid.Children.Count);
+
+            foreach (var item in buttons)
+            {
+                Button button0 = new Button()
+                {
+                    
+                    Content = item.Content,
+                    Tag = (TagReader.GetRow(item.Tag.ToString())
+                            + TagReader.GetCol(item.Tag.ToString()
+                            + TagReader.GetColor(item.Tag.ToString())
+                            + TagReader.GetValue(item.Tag.ToString())))
+                };
+
+                Grid.SetRow(button0, TagReader.GetRow(item.Tag.ToString()));
+                Grid.SetColumn(button0, TagReader.GetCol(item.Tag.ToString()));
+                
+                grid.Children.Add(item);
+            }
+        }
+
+        public static Card Read(string tag)
+        {
+            var res = new Card();
+            res.Position.row = GetRow(tag);
+            res.Position.col = GetCol(tag);
+
+            res.Color = GetColor(tag);
+
+            res.isRed = parseColorRed(res);
+
+            res.Value = int.Parse(GetValue(tag));
+            //todo TAG?
+            return res;
+        }
+
+        public static bool parseColorRed(Card res)
+        {
+            if ((res.Color == "S" || res.Color == "C"))
+                return false;
+            else
+                return true;
+        }
+
+        internal static List<Button> FindTail(string t, System.Windows.Controls.UIElementCollection uIElementCollection)
+        {
+            List<Button> res = new List<Button>();
+
+            int row = TagReader.GetRow(t);
+            int col = TagReader.GetCol(t);
+
+            foreach (Button item in uIElementCollection)
+            {
+                int btnRow = TagReader.GetRow(item.Tag.ToString());
+                int btnCol = TagReader.GetCol(item.Tag.ToString());
+
+                if (btnRow > row && btnCol == col)
+                {
+                    res.Add(item);
+                } 
+            }
+            //
+
+            //
+            return res.OrderByDescending(c => GetValue(c.Tag.ToString())).ToList();
+        }
+
+        internal static int GetCol(string t)
+        {
+            int res = 0;
+            try
+            {
+                int row = GetRow(t);
+                if (row > 9)
+                {
+                    res = int.Parse(t.Substring(2, 1));
+                }
+                else
+                {
+                    res = int.Parse(t.Substring(1, 1));
+                }
+            }
+            catch { }
+
+            return res;
+        }
+
+        internal static int GetRow(string t)
+        {
+            int row = 0;
+            try
+            {
+                string lead = string.Empty;
+                int sep = t.IndexOfAny(new char[] { 'D', 'S', 'H', 'C' });
+
+                for (int i = 0; i < sep; i++)
+                {
+                    lead += t[i];
+                }
+
+                lead = lead.Substring(0, lead.Length - 1);
+
+                row = int.Parse(lead);
+            }
+            catch { }
+
+            return row;
+        }
+
+        internal static string GetColor(string t)
+        {
+            try
+            {
+                int row = GetRow(t);
+                if (row > 9)
+                {
+                    t = t.Substring(3, 1);
+                }
+                else
+                {
+                    t = t.Substring(2, 1);
+                }
+            }
+            catch { }
+
+            return t;
+        }
+
+        internal static string GetValue(string t)
+        {
+            string resStr = string.Empty;
+
+            try
+            {
+                int row = GetRow(t);
+                if (row > 9)
+                {
+                    string lead = t.Substring(4, 1);
+                    if (lead == "1" && lead.Length == 2)
+                    {
+                        row = int.Parse(t.Substring(0, 2));
+                    }
+                    else
+                    {
+                        row = int.Parse(t.Substring(0, 1));
+                    }
+
+                    resStr = t.Substring(4, t.Length - 4);
+                }
+                else
+                {
+                    resStr = t.Substring(3, t.Length - 3);
+                }
+            }
+            catch { }
+
+            return resStr;
+            //
+        }
+
+        internal static string GenTag(string ot, int r, int c)
+        {
+            StringBuilder res = new StringBuilder(r.ToString() + c.ToString() +
+                    GetColor(ot) + GetValue(ot));
+            return res.ToString();
+        }
+
+        internal static List<Card> PopulateCards(UIElementCollection uIElementCollection)
+        {
+            List<Card> cards = new List<Card>();
+
+            foreach (Button item in uIElementCollection)
+            {
+                Card card = new Card();
+
+                card.Value = int.Parse(GetValue(item.Tag.ToString()));
+                card.Color = GetColor(item.Tag.ToString());
+                card.isRed = parseColorRed(card);
+
+                card.Position.col = GetCol(item.Tag.ToString());
+                card.Position.row = GetRow(item.Tag.ToString());
+
+                cards.Add(card);
+            }
+
+            //set top cards
+            for (int k = 1; k <= 8; k++)
+            {
+                Card topCard = new Card();
+                topCard.Position.row = 1; 
+
+                foreach (Card c in cards)
+                {
+                    if (c.Position.col == k)
+                    {
+                        if (c.Position.row > topCard.Position.row)
+                        {
+                            topCard = c;
+                        }
+                    }
+                }
+
+                topCard.isTop = true;
+            }
+
+            //set top mius 1 cards
+            for (int k = 1; k <= 8; k++)
+            {
+                Card topMinusOneCard = new Card();
+                topMinusOneCard.Position.row = 1;
+
+                foreach (Card c in cards)
+                {
+                    if (c.Position.col == k)
+                    {
+                        if (c.isTop)
+                        {
+                            break;
+                        }
+
+                        if (c.Position.row > topMinusOneCard.Position.row)
+                        {
+                            topMinusOneCard = c;
+                        }
+                    }
+                }
+
+                topMinusOneCard.isTopMinusOne = true;
+            }
+
+            return cards;
+        }
+    }
+}
---
+++ FreeCellMetrics/MainWindow.xaml.cs
@@ -0,0 +1,297 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using FreeCellMetrics.Classes;
+
+namespace FreeCellMetrics
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //new Game().Generate();
+            //TODO
+
+            this.Cards = new List<Card>();
+
+        }
+
+        public List<Card> Cards
+        {
+            get;
+            set;
+        }
+
+        private static double total;
+        private string CalculateStats(Card c)
+        {
+            StringBuilder resStr = new StringBuilder();
+
+            this.Cards = TagReader.PopulateCards(this.grid.Children);
+            
+            var results = Cards.Where(s => s.Value == c.Value + 1
+                && s.isRed == !c.isRed && s.isTop).ToList();
+
+            resStr.Append(results.Count + " / 13 ");
+
+            double stat = results.Count / 13.0;
+
+            results = Cards.Where(s => s.Value == c.Value + 1
+                && s.isRed == !c.isRed && s.isTopMinusOne).ToList();
+
+            if (results.Count > 0)
+            {
+                resStr.Append(results.Count + " / 26 ");
+
+                stat += results.Count / 26.0;
+            }
+
+            total += stat;
+            resStr.Append(" " + stat.ToString("0.00") + " ");
+
+            return resStr.ToString();
+        }
+
+        /// <summary>
+        /// button drag drop
+        /// </summary>
+        List<double> rowsheight = new List<double>();
+        List<double> columnswidth = new List<double>();
+        Rect[,] rects = null;
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            double rowstarno = 0;
+            for (int i = 0; i < grid.RowDefinitions.Count; i++)
+            {
+                if (grid.RowDefinitions[i].Height.IsStar)
+                {
+                    rowstarno += grid.RowDefinitions[i].Height.Value;
+                }
+            }
+            for (int i = 0; i < grid.RowDefinitions.Count; i++)
+            {
+                GridLength length = grid.RowDefinitions[i].Height;
+                if (length.IsAbsolute)
+                {
+                    rowsheight.Add(length.Value);
+                }
+                else if (length.IsStar)
+                {
+                    double rowheight = (grid.RowDefinitions[i].Height.Value / rowstarno) * grid.ActualHeight;
+                    rowsheight.Add(rowheight);
+                }
+            }
+            double columnstarno = 0;
+            for (int i = 0; i < grid.ColumnDefinitions.Count; i++)
+            {
+                if (grid.ColumnDefinitions[i].Width.IsStar)
+                {
+                    columnstarno += grid.ColumnDefinitions[i].Width.Value;
+                }
+            }
+            for (int i = 0; i < grid.ColumnDefinitions.Count; i++)
+            {
+                GridLength length = grid.ColumnDefinitions[i].Width;
+                if (length.IsAbsolute)
+                {
+                    columnswidth.Add(length.Value);
+                }
+                else if (length.IsStar)
+                {
+                    double columnheight = (grid.ColumnDefinitions[i].Width.Value / columnstarno) * grid.ActualWidth;
+                    columnswidth.Add(columnheight);
+                }
+            }
+            rects = new Rect[rowsheight.Count, columnswidth.Count];
+            double yvalue = 0;
+            for (int i = 0; i < rowsheight.Count; i++)
+            {
+                for (int j = 0; j < columnswidth.Count; j++)
+                {
+                    rects[i, j] = new Rect() { Y = yvalue, Height = rowsheight[i] };
+                }
+                yvalue += rowsheight[i];
+            }
+            double xvalue = 0;
+            for (int j = 0; j < columnswidth.Count; j++)
+            {
+                for (int i = 0; i < rowsheight.Count; i++)
+                {
+                    rects[i, j].X = xvalue;
+                    rects[i, j].Width = columnswidth[j];
+                }
+                xvalue += columnswidth[j];
+            }
+
+        }
+
+        bool IsMouseDown = false, IsDraggingStarted = false, IsPopulated = false;
+        DependencyObject ClickedElement = null;
+
+        private void Grid_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            IsPopulated = false;
+            if (!e.Source.Equals(sender))
+            {
+                ClickedElement = e.Source as DependencyObject;
+                IsMouseDown = true;
+
+                //dirty hack
+                // this.TagText = ((Button)ClickedElement).Tag;
+                //
+            }
+        }
+
+        private void Grid_PreviewMouseMove(object sender, MouseEventArgs e)
+        {
+            if (!IsDraggingStarted && IsMouseDown)
+            {
+                DragDrop.DoDragDrop(sender as DependencyObject, ClickedElement, DragDropEffects.Move);
+            }
+        }
+
+        private void grid_Drop(object sender, DragEventArgs e)
+        {
+            Point droppedpoint = e.GetPosition(sender as IInputElement);
+            Grid grid = sender as Grid;
+
+            bool cellfound = false;
+
+            for (int i = 0; i < rects.GetLength(0); i++)
+            {
+                for (int j = 0; j < rects.GetLength(1); j++)
+                {
+                    if (rects[i, j].Contains(droppedpoint))
+                    {
+                        ClickedElement.SetValue(Grid.RowProperty, i);
+                        ClickedElement.SetValue(Grid.ColumnProperty, j);
+                        //dirty hack
+                        Button clickedBtn;
+
+                        if (ClickedElement is Image)
+                            clickedBtn = (Button)((Image)ClickedElement).Parent;
+                        else
+                            clickedBtn = (Button)(ClickedElement); //todo
+
+                        this.TagText = clickedBtn.Tag.ToString();
+
+                        string newTag = string.Empty;
+                        
+                        newTag = TagReader.GenTag(clickedBtn.Tag.ToString(), i, j);
+                          
+
+                        Button button0 = new Button()
+                        {
+                            Content = clickedBtn.Content,
+                            Background = clickedBtn.Background,
+                            Tag = (TagReader.GetRow(newTag.ToString())
+                                    + TagReader.GetCol(newTag).ToString()
+                                    + TagReader.GetColor(newTag)
+                                    + TagReader.GetValue(newTag))
+                        };
+
+                        Grid.SetRow(button0, TagReader.GetRow(newTag.ToString()));
+                        Grid.SetColumn(button0, TagReader.GetCol(newTag.ToString()));
+
+                        this.grid.Children.Add(button0);
+
+                        if (TagReader.GetRow(this.TagText) != 0)
+                        {
+                            //find tail 14S4
+                            List<Button> tail = TagReader.FindTail(this.TagText, this.grid.Children);
+                            i = TagReader.GetRow(newTag);
+                            foreach (var el in tail)
+                            {
+                                Button button = new Button()
+                                {
+                                    Content = el.Content,
+                                    Background = el.Background,
+                                    Tag = ((++i).ToString()
+                                            + TagReader.GetCol(newTag).ToString()
+                                            + TagReader.GetColor(el.Tag.ToString())
+                                            + TagReader.GetValue(el.Tag.ToString()))
+                                };
+
+                                Grid.SetRow(button, TagReader.GetRow(button.Tag.ToString()));
+                                Grid.SetColumn(button, TagReader.GetCol(button.Tag.ToString()));
+                                //button.Click += new RoutedEventHandler(button_Click);
+                                this.grid.Children.Add(button);
+                                this.grid.Children.Remove(el);
+                            }
+                        }
+
+
+                        //
+                        cellfound = true;
+
+                        //set title
+                        
+                            var resStr = new StringBuilder();
+                            total = 0;
+
+                            foreach (Button item in grid.Children)
+                            {
+                                if (TagReader.GetRow(item.Tag.ToString()) == 0)
+                                {
+                                    if (TagReader.GetCol(item.Tag.ToString()) == 5
+                                    || TagReader.GetCol(item.Tag.ToString()) == 6
+                                    || TagReader.GetCol(item.Tag.ToString()) == 7
+                                    || TagReader.GetCol(item.Tag.ToString()) == 8)
+                                    {
+                                        resStr.Append(CalculateStats(TagReader.Read(item.Tag.ToString())));
+                                    }
+                                }
+                            }
+
+                            this.Title = total.ToString(".00") + " " + resStr.ToString();
+                            //todo if (TagReader.GetCol(newTag) == 1) +1
+                            //
+                        
+
+                        if (!IsPopulated)
+                        {
+                            TagReader.Populate(TagText, grid);
+                            IsPopulated = true;                           
+                        }
+                        //
+
+                        break;
+                    }
+                }
+                if (cellfound)
+                {
+                    break;
+                }
+            }
+
+            
+            IsDraggingStarted = false;
+            IsMouseDown = false;
+
+           
+        }
+
+        string tagText = string.Empty;
+        public string TagText
+        {
+            get { return this.tagText; }
+            set
+            {
+                this.tagText = value.ToString();
+            }
+        }
+    }
+}
